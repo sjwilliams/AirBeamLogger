9df49181c1df54637bdf4fa515b8edf1721b9622
Josh Williams <josh@joshwilliams.com>
2019-11-04 21:14:34 -0500

    pm 2.5 chart sorta working

--- html/index.php
+++ html/index.php
@@ -12,31 +12,42 @@
 
 <body>
   <h1>AirBeam Data Logger</h1>
-  <p><button id="start">Start Data Logger</button></p>
-  <p><button id="stop" disabled>Stop Data Logger</button></p>
-  <p><button id="clear" disabled>Clear Data</button></p>
-  <p><button id="shutdown">Shutdown</button></p>
 
-  <form id="settings">
-    <p>
-      <input id="delay" type="number" max="1440" min="1" value="<?php echo($settings["delay"] / 60) ?>">
-      <button onclick="update_settings(0)">Set Sample Rate (min)</button>
-    </p>
-    <p>
-      <input id="date" type="checkbox" value="date" <?php if ( $settings["date"] == 1 ) echo("checked") ?>
-        onclick="update_settings(1)">
-      Enable date and time stamp.
-    </p>
-    <p>
-      <input id="ntp" type="checkbox" value="ntp" <?php if ( $settings["ntp"] == 1 ) echo("checked") ?>
-        onclick="update_settings(2)">
-      Enable NTP time sync.
-    </p>
-  </form>
+  <section id="controls">
+    <p><button id="start">Start Data Logger</button></p>
+    <p><button id="stop" disabled>Stop Data Logger</button></p>
+    <p><button id="clear" disabled>Clear Data</button></p>
+    <p><button id="shutdown">Shutdown</button></p>
+  
+    <form id="settings">
+      <p>
+        <input id="delay" type="number" max="1440" min="1" value="<?php echo($settings["delay"] / 60) ?>">
+        <button onclick="update_settings(0)">Set Sample Rate (min)</button>
+      </p>
+      <p>
+        <input id="date" type="checkbox" value="date" <?php if ( $settings["date"] == 1 ) echo("checked") ?>
+          onclick="update_settings(1)">
+        Enable date and time stamp.
+      </p>
+      <p>
+        <input id="ntp" type="checkbox" value="ntp" <?php if ( $settings["ntp"] == 1 ) echo("checked") ?>
+          onclick="update_settings(2)">
+        Enable NTP time sync.
+      </p>
+    </form>
+  </section>
 
-  <p><a href="output.csv">Data File</a></p>
+
+  <section id="data">
+    <div id="chart">
+      <svg width="960" height="500"></svg>
+    </div>
+    <p><a href="output.csv">Data File</a></p>
+  </section>
+
 
   <script src="jquery.min.js"></script>
+  <script src="d3.js"></script>
   <script src="script.js"></script>
 </body>
 </html>
--- html/script.js
+++ html/script.js
@@ -1,5 +1,28 @@
 $(function () {
 
+  const $startBtn = $('#start');
+  const $stopBtn = $('#stop');
+  const $clearBtn = $('#clear');
+
+  const celsius2fahrenheit = (celsius) => {
+    return (celsius * (9 / 5)) + 32;
+  };
+
+  const movingAverage = (values, N) => {
+    let i = 0;
+    let sum = 0;
+    const means = new Float64Array(values.length).fill(NaN);
+    for (let n = Math.min(N - 1, values.length); i < n; ++i) {
+      sum += values[i];
+    }
+    for (let n = values.length; i < n; ++i) {
+      sum += values[i];
+      means[i] = sum / N;
+      sum -= values[i - N + 1];
+    }
+    return means;
+  };
+
   // Create AJAX functions to control the data logger.
   function start() {
     $.ajax({
@@ -89,13 +112,123 @@ $(function () {
     }
   }
 
+  const updateChart = (data) => {
+    const svg = d3.select("svg");
+    
+
+    const margin = {
+      top: 20,
+      right: 20,
+      bottom: 10,
+      left: 20
+    };
+    const width = +svg.attr("width") - margin.left - margin.right;
+    const height = +svg.attr("height") - margin.top - margin.bottom;
+
+
+    const g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    const parseTime = d3.timeParse("%Y/%m/%d %H:%M:%S");
+
+    // prep data
+    const processedData = data.map((d) => {
+      const celsius = parseInt(d['Temperature (C)']);
+      return {
+        celsius,
+        date: parseTime(d.Time),
+        fahrenheit: Math.round(celsius2fahrenheit(celsius)),
+        pm25: parseInt(d['PM2.5']),
+        pm10: parseInt(d['PM10']),
+        pm1: parseInt(d['PM1']),
+        rh: parseInt(d['%RH']),
+        value: parseInt(d['PM2.5']),
+      };
+    });
+
+    // const x = d3.scaleTime()
+    //   .rangeRound([0, width]);
+
+    const x = d3.scaleUtc()
+      .domain(d3.extent(processedData, d => d.date))
+      .range([margin.left, width - margin.right]);
+
+    // const y = d3.scaleLinear()
+    //   .rangeRound([height, 0]);
+
+    const y = d3.scaleLinear()
+      .domain([0, d3.max(processedData, d => d.value)]).nice()
+      .range([height - margin.bottom, margin.top]);
+
+    const xAxis = g => g
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
+
+    const yAxis = g => g
+      .attr("transform", `translate(${margin.left},0)`)
+      .call(d3.axisLeft(y))
+      .call(g => g.select(".domain").remove())
+      .call(g => g.select(".tick:last-of-type text").clone()
+        .attr("x", 3)
+        .attr("text-anchor", "start")
+        .attr("font-weight", "bold")
+        .text(data.y));
+        
+    const line = d3.line()
+      .defined(d => !isNaN(d.value))
+      .x(d => x(d.date))
+      .y(d => y(d.value));
+
+
+
+
+
+    g.append("g")
+      .call(xAxis);
+
+    g.append("g")
+      .call(yAxis);
+
+    g.append("path")
+      .datum(processedData)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("d", line);
+
+    console.log(processedData);
+  };
+
+  const update = () => {
+    d3.json("/get_data.php").then(function (json) {
+      const hasData = !!json.data;
+
+      if (hasData) {
+        $clearBtn.removeAttr('disabled');
+        updateChart(json.data);
+      } else {
+        $clearBtn.addAttr('disabled');
+      }
+
+      // setTimeout(update, 10000);
+    });
+  };
+
   // Disable form submission, it's all handled via JavaScript.
   $('#settings').submit(function (e) {
     e.preventDefault();
     return false;
   });
 
-  $('#start').on('click', () => {
+  $startBtn.on('click', () => {
     console.log('click');
+    start();
   });
+
+  $stopBtn.on('click', () => {
+
+  });
+
+
+  update();
 });
